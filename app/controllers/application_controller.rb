4f26e303d2a0b79f53fe9cce4eef05f709fbb509
Justine Jones <justinejones91@gmail.com>
2014-11-12 21:20:22 -0500

    ready

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   	@current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
-  def ensure_logged_in
+  def ensure_login
   	unless current_user
   		flash[:alert] ="You must login"
   		redirect_to new_session_path
